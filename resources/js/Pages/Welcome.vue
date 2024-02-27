661ba7ad8ce5e7c651af42129b90e4c1a325e409
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-02-28 00:49:34 +0600

    modal comp wip

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,19 +5,41 @@
 
         <Button
             type="primary"
-            @click="() => {console.log('clicked')}"
+            @click="() => {state.showModal = !state.showModal;}"
             :icon="Edit"
-            disabled
         >
             Get Started
         </Button>
     </div>
+
+    <ModalComp
+        v-model:visible="state.showModal"
+        title="Welcome to your new app!"
+        width="500"
+        top="100"
+        :modal="true"
+        :lock-scroll="false"
+        :custom-class="'modal-class'"
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
+        :show-close="false"
+    >
+        <p>
+            This is a modal component.
+        </p>
+    </ModalComp>
 </template>
 
 <script setup>
 import { Head } from "@inertiajs/vue3";
 import Button from "@/Components/Button.vue";
 import {Edit} from "@element-plus/icons-vue";
+import {reactive} from "vue";
+import ModalComp from "@/Components/ModalComp.vue";
+
+const state = reactive({
+    showModal: false,
+});
 </script>
 
 <style scoped lang="scss">
