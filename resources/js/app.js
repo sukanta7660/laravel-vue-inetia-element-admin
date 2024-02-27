08a9eb932b10cbe40501b0c4af33688c16a35740
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-02-27 15:37:33 +0600

    element plus setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,19 @@
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/inertia-vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
             .use(plugin)
+            .use(ElementPlus)
             .mount(el)
     },
 });
