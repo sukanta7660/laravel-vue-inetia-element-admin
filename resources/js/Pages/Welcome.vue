ac9ce6292e37f16f1750bed8b3430fe0984d3a34
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-02-27 15:13:35 +0600

    inertia configured

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="test-class">
+        <h1>
+            Welcome to your new Inertia app! With Vue 3 and Vite ⚡️and laravel 10.
+        </h1>
+    </div>
+</template>
+
+<style lang="scss">
+.test-class {
+    h1 {
+        color: red;
+        text-align: center;
+    }
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+        extensions: ['.js', '.vue', '.json', '.scss'],
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/assets/scss/main";`,
+            },
+        }
+    },
 });
