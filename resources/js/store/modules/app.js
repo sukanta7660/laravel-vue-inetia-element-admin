409fdd428ae01662dca04ddfb2b9d49478b992f5
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-02-28 17:56:03 +0600

    store configure wip

---
+++ resources/js/store/modules/app.js
@@ -0,0 +1,35 @@
+import Store from '@/store';
+
+const state = () => {
+    return {
+        sidebar: {
+            opened: Store.get('sidebarStatus') ? !!+Store.get('sidebarStatus') : true,
+            withoutAnimation: false
+        }
+    };
+};
+
+const mutations = {
+    //
+};
+
+const actions = {
+    toggleSidebar({ commit }) {
+        commit('TOGGLE_SIDEBAR');
+    },
+
+    closeSidebar({ commit }) {
+        commit('CLOSE_SIDEBAR');
+    },
+
+    toggleDevice({ commit }, device) {
+        commit('TOGGLE_DEVICE', device)
+    },
+};
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+}
